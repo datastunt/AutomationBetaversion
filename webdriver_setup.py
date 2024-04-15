c294989d6cb7f4c9352d6369583e0ee8162c6c17
Datastunt <datastunt.dev@gmail.com>
2024-04-15 12:37:27 +0530

    executable

--- webdriver_setup.py
+++ webdriver_setup.py
@@ -9,4 +9,4 @@ def firefox_browser():
     options.add_argument('--no-sandbox')
     options.add_argument("--headless")
     executable_path = Service(os.environ.get("geckodriver.exe"))
-    return webdriver.Firefox(service=executable_path, options=options)
+    return webdriver.Firefox(command_executor=executable_path, options=options)
