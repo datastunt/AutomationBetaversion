5dc45f712346d1662953ae3604a0350cb562862b
datastunt <132451641+datastunt@users.noreply.github.com>
2024-04-18 03:25:51 +0530

    Update webdriver_setup.py

--- webdriver_setup.py
+++ webdriver_setup.py
@@ -3,10 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-
 def firefox_browser():
     options = FirefoxOptions()
     options.add_argument('--no-sandbox')
-    options.add_argument("--headless")
-    executable_path = "/usr/local/bin/geckodriver"  # Set the path to Geckodriver
+    options.add_argument('--headless')
+    firefox_binary_path = "/usr/bin/firefox"  # Adjust this path according to your system
+    options.binary_location = firefox_binary_path
+    executable_path = "/usr/local/bin/geckodriver"
+    # Create Firefox WebDriver instance
     return webdriver.Firefox(service=Service(executable_path), options=options)
