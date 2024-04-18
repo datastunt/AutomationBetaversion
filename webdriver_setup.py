295758bd7d0493a8cd980a0188bef04082fd49c8
Datastunt <datastunt.dev@gmail.com>
2024-04-18 18:58:30 +0530

    firefox nondownloader

--- webdriver_setup.py
+++ webdriver_setup.py
@@ -7,28 +7,10 @@ def firefox_browser():
     options = FirefoxOptions()
     options.add_argument('--no-sandbox')
     options.add_argument("--headless")
-    
-    firefox_binary_path = "/usr/bin/firefox"  # Adjust this path according to your system
-    options.binary_location = firefox_binary_path
-    
-    geckodriver_path = "/usr/local/bin/geckodriver"  # Adjust this path according to your system
+
+    # Adjust the Geckodriver path according to your Docker setup
+    geckodriver_path = "/usr/local/bin/geckodriver"
+
     service = Service(geckodriver_path)  # Set the path to Geckodriver
-    
+
     return webdriver.Firefox(service=service, options=options)
-
-
-
-# import os
-# from selenium import webdriver
-# from selenium.webdriver.firefox.service import Service
-# from selenium.webdriver.firefox.options import Options as FirefoxOptions
-
-# def firefox_browser():
-#     options = FirefoxOptions()
-#     options.add_argument('--no-sandbox')
-#     options.add_argument('--headless')
-#     firefox_binary_path = "/usr/bin/firefox"  # Adjust this path according to your system
-#     options.binary_location = firefox_binary_path
-#     executable_path = "/usr/local/bin/geckodriver"
-#     # Create Firefox WebDriver instance
-#     return webdriver.Firefox(service=Service(executable_path), options=options)
